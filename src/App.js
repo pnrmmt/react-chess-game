754e41468ff325672cf597065b57432f3221aa90
pinar mamat <pnmamat35@gmail.com>
2023-12-01 15:59:52 +0300

    dosyalar eklendi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Chessboard} from 'react-chessboard';
+import {Chess} from 'chess.js'
+import { useState } from 'react';
 
 function App() {
+
+  const [game,setGame] = useState(new Chess());
+  
+  function safeGameMutate(modify){
+    setGame((g)=>{
+      const update = {...g}
+      modify(update)
+      return update;
+    })
+  }
+
+  function makeRandomMove(){
+    const possibleMove = game.move();
+
+    if(game.game_over() || game.in_draw() || possibleMove.length===0) return;
+
+
+    const randomIndex = Math.floor(Math.random() * possibleMove.length)
+
+    safeGameMutate((game)=>{
+      game.move(possibleMove[randomIndex])
+    })
+  }
+
+  function onDrop(source,target){
+    let move = null;
+    safeGameMutate((game)=>{
+      move = game.move({
+        from:source,
+        to:target,
+        promotion:'q'
+      })
+    })
+
+    if(move==null) return false
+
+    setTimeout(makeRandomMove, 200);
+    return true
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+       <Chessboard 
+       position={game.fen()}
+       onPieceDrop={onDrop}
+       />
+      
     </div>
+   
   );
 }
 
